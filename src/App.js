6ce16a1f1421c710d1d316262dfb3c63879d7e59
tarik.subasi <tarik.subasi@hotmail.com>
2022-06-05 09:18:40 +0300

    13 - useState Hook and re-draw UI

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Main from "./component-tree/Main";
 import Profile from "./component-tree/Profile";
 import Search from "./component-tree/Search";
@@ -5,17 +6,29 @@ import User from "./component-tree/User";
 import "./App.css";
 
 function App() {
+  const [appName, setAppName] = useState("default");
+
+  const onChange = (e) => {
+    setAppName(e.target.value);
+  };
+
   return (
     <div className="App mt-5">
-      <h2
-        style={{
-          textAlign: "center",
-        }}
+      <div className="d-flex justify-content-center align-items-center flex-column">
+        <h2
+          style={{
+            textAlign: "center",
+          }}
+        >
+          {appName}
+        </h2>
+        <input type="text" onChange={onChange} />
+      </div>
+
+      <div
+        className="container border p-4 shadow-sm"
+        style={{ height: "80vh" }}
       >
-        App
-      </h2>
-      <div className="container border p-4 shadow-sm"
-      style={{ height : "80vh"}}>
         <div className="row">
           <div className="col">
             <Profile>
@@ -25,7 +38,10 @@ function App() {
           <div className="col">
             <Main />
           </div>
-          <div className="col "   style={{ overflowY: "scroll",maxHeight: "600px"}}>
+          <div
+            className="col "
+            style={{ overflowY: "scroll", maxHeight: "600px" }}
+          >
             <Search />
           </div>
         </div>
