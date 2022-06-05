446558a77daf2f5380df6be4248e0631af1a9739
tarik.subasi <tarik.subasi@hotmail.com>
2022-06-05 19:53:53 +0300

    18 - Immutable state update

--- src/component-tree/Main.js
+++ src/component-tree/Main.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AppStateContext } from "../App";
 import { getRandomColor } from "../utils/utility";
 
@@ -10,12 +10,45 @@ const Main = () => {
     setBackground(randomColor);
   };
 
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    password: "",
+  });
+
+  const onUserChange = (e) => {
+    setUserInfo((state) => {
+      return { ...state, name: e.target.value };
+    });
+  };
+
+  const onPassChange = (e) => {
+    setUserInfo((state) => {
+      return { ...state, password: e.target.value };
+    });
+  };
+
   return (
     <div className=" d-flex justify-content-center align-items-center flex-column">
       <h3> Main</h3>
       <button className="mt-2 btn btn-primary" onClick={onChangeBackground}>
         Set Random Background Color
       </button>
+      <label className="mt-2">UserName</label>
+      <input
+        onChange={onUserChange}
+        value={userInfo.name}
+        type="text"
+        className="mt-1 form-control"
+        style={{ width: "250px" }}
+      />
+      <label>password</label>
+      <input
+        value={userInfo.password}
+        type="password"
+        className="mt-1 form-control"
+        style={{ width: "250px" }}
+        onChange={onPassChange}
+      />
     </div>
   );
 };
